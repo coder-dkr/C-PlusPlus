bb6b7d63fedea5a600a6a5f6706f28f1420660e3
coder-dkr <48358072+coder-dkr@users.noreply.github.com>
2019-06-28 03:09:03 +0530

    Create BasicInputOutput.cpp
    Basic input output code

---
+++ BasicInputOutput.cpp
@@ -0,0 +1,11 @@
+//Print the inputed number
+#include<iostream>
+using namespace std;
+
+int main () {
+    int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+    cout<<"Number entered by you is "<<num;
+    return 0;
+}
